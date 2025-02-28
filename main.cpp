b9c3023a8b1c89e55eef00f1cffa3544a0cc11c9
Vyacheslav Batychkov <128066686+parzij@users.noreply.github.com>
2025-02-28 19:51:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,59 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
+// ANSI-коды для цветов
+#define ORANGE "\033[38;5;208m"
+#define BLUE "\033[34m"
+#define RESET "\033[0m"
+
 void drawBoard(char board[]) {
-    cout << " " << board[0] << " \033[1;36m?\033[0m " << board[1] << " \033[1;36m?\033[0m " << board[2] << endl;
-    cout << "\033[1;36m???????????\033[0m" << endl;
-    cout << " " << board[3] << " \033[1;36m?\033[0m " << board[4] << " \033[1;36m?\033[0m " << board[5] << endl;
-    cout << "\033[1;36m???????????\033[0m" << endl;
-    cout << " " << board[6] << " \033[1;36m?\033[0m " << board[7] << " \033[1;36m?\033[0m " << board[8] << endl;
+    cout << ORANGE << " " << BLUE << board[0] << ORANGE << " | " << BLUE << board[1] << ORANGE << " | " << BLUE << board[2] << ORANGE << " " << RESET << endl;
+    cout << ORANGE << "-----------" << RESET << endl;
+    cout << ORANGE << " " << BLUE << board[3] << ORANGE << " | " << BLUE << board[4] << ORANGE << " | " << BLUE << board[5] << ORANGE << " " << RESET << endl;
+    cout << ORANGE << "-----------" << RESET << endl;
+    cout << ORANGE << " " << BLUE << board[6] << ORANGE << " | " << BLUE << board[7] << ORANGE << " | " << BLUE << board[8] << ORANGE << " " << RESET << endl;
 }
-// Функция, которая проверяет, является ли данный символ победным
 
 bool isWinner(char board[], char symbol) {
-    // Проверяем все возможные комбинации, которые могут привести к победе
-    if ((board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
-        (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
-        (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
-        (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
-        (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
-        (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
-        (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
-        (board[2] == symbol && board[4] == symbol && board[6] == symbol)) {
-        return true;
+    const int winPatterns[8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Rows
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columns
+        {0, 4, 8}, {2, 4, 6}             // Diagonals
+    };
+    for (const auto &pattern : winPatterns) {
+        if (board[pattern[0]] == symbol && board[pattern[1]] == symbol && board[pattern[2]] == symbol) {
+            return true;
+        }
     }
     return false;
 }
-// Функция, которая проверяет, остались ли еще свободные ячейки на игровой доске
+
 bool isBoardFull(char board[]) {
     for (int i = 0; i < 9; i++) {
-        if (board[i] == ' ') {
-            return false;
-        }
+        if (board[i] == ' ') return false;
     }
     return true;
 }
-// Функция, которая реализует ход компьютера
+
 void computerMove(char board[], char computerSymbol, char playerSymbol) {
-    // Проходим по всем ячейкам доски и пытаемся поставить символ компьютера в каждую свободную ячейку
     for (int i = 0; i < 9; i++) {
         if (board[i] == ' ') {
             board[i] = computerSymbol;
-            // Если компьютер может выиграть на следующем ходу, то ставим символ компьютера в эту ячейку и выходим из функции
-            if (isWinner(board, computerSymbol)) {
+            if (isWinner(board, computerSymbol)) return;
+            board[i] = ' ';
+        }
+    }
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ') {
+            board[i] = playerSymbol;
+            if (isWinner(board, playerSymbol)) {
+                board[i] = computerSymbol;
                 return;
             }
-            // Иначе, если игрок может выиграть на следующем х оду, то ставим символ игрока в эту ячейку, чтобы предотвратить его победу, и выходим из функции
-            else if (isWinner(board, playerSymbol)) {
-                board[i] = playerSymbol;
-                return;
-            }
-            else {
-                board[i] = ' ';
-                }
+            board[i] = ' ';
         }
     }
     do {
@@ -63,48 +64,49 @@ void computerMove(char board[], char computerSymbol, char playerSymbol) {
         }
     } while (true);
 }
-    void playerMove(char board[], char playerSymbol) {
-        int move;
-        do {
-            cout << "\033[1;34mEnter your move (0-8): \033[0m";
-            cin >> move;
-            }
-        while (board[move] != ' ');
-            board[move] = playerSymbol;
+
+void playerMove(char board[], char playerSymbol) {
+    int move;
+    do {
+        cout << BLUE << "Enter your move (0-8): " << RESET;
+        cin >> move;
+    } while (move < 0 || move >= 9 || board[move] != ' ');
+    board[move] = playerSymbol;
 }
+
 void playGame() {
-    char board[9] = { ' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+    char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     char playerSymbol = 'X';
     char computerSymbol = 'O';
 
-    if (rand() % 2 == 0) {    // Определяем, кто будет ходить первым случайным образом
-        cout << "\033[1;34mComputer start the first.\033[0m" << endl;
+    if (rand() % 2 == 0) {
+        cout << BLUE << "Computer starts first." << RESET << endl;
         computerMove(board, computerSymbol, playerSymbol);
+    } else {
+        cout << BLUE << "You start first." << RESET << endl;
     }
-    else {
-        cout << "\033[1;34mYou start the first.\033[0m" << endl;
-    }
-    while (!isBoardFull(board)) {    // Игра продолжается до тех пор, пока не закончится доска или кто-то не выиграет
+
+    while (!isBoardFull(board)) {
         drawBoard(board);
         playerMove(board, playerSymbol);
         if (isWinner(board, playerSymbol)) {
             drawBoard(board);
-            cout << "\033[1;34mYou win!\033[0m" << endl;
+            cout << BLUE << "You win!" << RESET << endl;
             return;
         }
         computerMove(board, computerSymbol, playerSymbol);
         if (isWinner(board, computerSymbol)) {
             drawBoard(board);
-            cout << "\033[1;34mComputer wins!\033[0m" << endl;
+            cout << BLUE << "Computer wins!" << RESET << endl;
             return;
         }
     }
     drawBoard(board);
-    cout << "\033[1;34mDraw\033[0m" << endl;
+    cout << BLUE << "Draw!" << RESET << endl;
 }
 
 int main() {
-    srand(time(NULL));    // Инициализация генератора случайных чисел
-    playGame();    // Запуск игры
+    srand(time(NULL));
+    playGame();
     return 0;
 }
